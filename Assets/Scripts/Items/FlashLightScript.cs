ba44a8e5bd2ae86e785ae0770a55e2ddae79bb74
NalinKadariya <nali0074@edu.nextkbh.dk>
2025-04-26 13:37:20 +0200

    Added constrations to ricer

--- Assets/Scripts/Items/FlashLightScript.cs
+++ Assets/Scripts/Items/FlashLightScript.cs
@@ -1,16 +1,84 @@
 using UnityEngine;
+using CharacterControl.Manager;
 
-public class FlashLightScript : MonoBehaviour
+namespace GameItemsNameSpace.FlashLightItem
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class CameraFlashlightController : MonoBehaviour
     {
-        
-    }
+        [Header("Flashlight Settings")]
+        [SerializeField] private Light _flashlight;
+        [SerializeField] private float[] _intensitySteps = { 0f, 2f, 5f, 10f };
+        [SerializeField] private int _startingStepIndex = 2; // Start at index 2
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("Audio Settings")]
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private AudioClip _intensityUpClip;
+        [SerializeField] private AudioClip _intensityDownClip;
+
+        [Header("References")]
+        [SerializeField] private InputManager _inputManager;
+
+        private int _currentStepIndex;
+
+        private void Start()
+        {
+            _currentStepIndex = Mathf.Clamp(_startingStepIndex, 0, _intensitySteps.Length - 1);
+
+            if (_flashlight != null)
+            {
+                _flashlight.intensity = _intensitySteps[_currentStepIndex];
+                _flashlight.enabled = true;
+            }
+        }
+
+        private void Update()
+        {
+            if (_inputManager == null) return;
+
+            HandleIntensityControl();
+        }
+
+        private void HandleIntensityControl()
+        {
+            if (_inputManager.IncreaseFlashlightIntensity)
+            {
+                _inputManager.IncreaseFlashlightIntensity = false;
+
+                if (_currentStepIndex < _intensitySteps.Length - 1)
+                {
+                    _currentStepIndex++;
+                    UpdateFlashlightIntensity();
+                    PlayOneShot(_intensityUpClip);
+                }
+            }
+
+            if (_inputManager.DecreaseFlashlightIntensity)
+            {
+                _inputManager.DecreaseFlashlightIntensity = false;
+
+                if (_currentStepIndex > 0)
+                {
+                    _currentStepIndex--;
+                    UpdateFlashlightIntensity();
+                    PlayOneShot(_intensityDownClip);
+                }
+            }
+        }
+
+        private void UpdateFlashlightIntensity()
+        {
+            if (_flashlight != null)
+            {
+                _flashlight.intensity = _intensitySteps[_currentStepIndex];
+            }
+        }
+
+        private void PlayOneShot(AudioClip clip)
+        {
+            if (_audioSource != null && clip != null)
+            {
+                _audioSource.PlayOneShot(clip);
+            }
+        }
     }
 }
--- Assets/Scripts/PlayerController/InputManager.cs
+++ Assets/Scripts/PlayerController/InputManager.cs
@@ -11,11 +11,17 @@ namespace CharacterControl.Manager
         public Vector2 Move {get; private set;}
         public Vector2 Look {get; private set;}
         public bool Run {get; private set;}
+        // Flashlight
+        public bool IncreaseFlashlightIntensity { get; set; }
+        public bool DecreaseFlashlightIntensity { get; set; }
+
 
         private InputActionMap _currentMap;
         private InputAction _moveAction;
         private InputAction _lookAction;
         private InputAction _runAction;
+        private InputAction _increaseIntensityAction;
+        private InputAction _decreaseIntensityAction;
 
         private void Awake()
         {
@@ -24,13 +30,23 @@ namespace CharacterControl.Manager
             _lookAction = _currentMap.FindAction("Look");
             _runAction = _currentMap.FindAction("Run");
 
+            // Flashlight actions
+            _increaseIntensityAction = _currentMap.FindAction("IncreaseFlashlightIntensity");
+            _decreaseIntensityAction = _currentMap.FindAction("DecreaseFlashlightIntensity");
+
+            // Add
             _moveAction.performed += OnMove;
             _lookAction.performed += OnLook;
             _runAction.performed += OnRun;
+            _increaseIntensityAction.performed += OnIncreaseFlashlightIntensity;
+            _decreaseIntensityAction.performed += OnDecreaseFlashlightIntensity;
 
+            // Release
             _moveAction.canceled += OnMove;
             _lookAction.canceled += OnLook;
             _runAction.canceled += OnRun;
+            _increaseIntensityAction.canceled += OnIncreaseFlashlightIntensity;
+            _decreaseIntensityAction.canceled += OnDecreaseFlashlightIntensity;
         }
 
         private void OnMove(InputAction.CallbackContext context)
@@ -58,6 +74,15 @@ namespace CharacterControl.Manager
             _currentMap.Disable();
         }
 
+        private void OnIncreaseFlashlightIntensity(InputAction.CallbackContext context)
+        {
+           IncreaseFlashlightIntensity = context.ReadValueAsButton();
+        }
+
+        private void OnDecreaseFlashlightIntensity(InputAction.CallbackContext context)
+        {
+            DecreaseFlashlightIntensity = context.ReadValueAsButton();
+        }
 
     }
 }
