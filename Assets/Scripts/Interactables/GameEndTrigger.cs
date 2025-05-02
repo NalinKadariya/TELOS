3a81572ca3d48715ffa39ec324682c26a2f8f6e4
NalinKadariya <nali0074@edu.nextkbh.dk>
2025-05-02 08:50:33 +0200

    Semi final

--- Assets/Scripts/Interactables/GameEndTrigger.cs
+++ Assets/Scripts/Interactables/GameEndTrigger.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
-using UnityEngine.Playables;
 using PlayerSystem;
 using Story;
 using System.Collections.Generic;
+using CharacterControl.PlayerControl;
 
 public class GameEndTrigger : MonoBehaviour
 {
@@ -10,34 +10,47 @@ public class GameEndTrigger : MonoBehaviour
     [SerializeField] private string _playerTag = "Player";
     [SerializeField] private string _endGameItemID = "GameFinished";
 
-    [Header("Timelines")]
-    [SerializeField] private PlayableDirector _gameFinishedTimeline;
-    [SerializeField] private PlayableDirector _gameOverTimeline;
+
+    [Header("UI & Audio")]
+    [SerializeField] private Animator _fadeAnimator;
+    [SerializeField] private AudioSource _endingMusic;
 
     private bool _hasTriggered = false;
+    private PlayerController _playerController;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (_hasTriggered || other.CompareTag(_playerTag) == false)
+        if (_playerController == null)
+            _playerController = other.GetComponent<PlayerController>();
+        
+        _playerController.enabled = false;
+
+        _playerController.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        _playerController.GetComponent<Animator>().enabled = false;
+        if (_hasTriggered || !other.CompareTag(_playerTag))
             return;
 
         _hasTriggered = true;
 
         if (PlayerInventory.Instance.HasItem(_endGameItemID))
         {
-            if (_gameFinishedTimeline != null)
-                _gameFinishedTimeline.Play();
-
             DialogManager.PlayDialogsQueue(new List<string> { "GameWon 1", "GameWon 2" });
-            Debug.Log("Ending 1.");
+            Debug.Log("Ending: Game Won");
         }
         else
         {
-            if (_gameOverTimeline != null)
-                _gameOverTimeline.Play();
-
             DialogManager.PlayDialogsQueue(new List<string> { "Lost 1", "Lost 2" });
-            Debug.Log("Ending 2.");
+            Debug.Log("Ending: Game Over");
+        }
+
+        if (_fadeAnimator != null)
+            _fadeAnimator.SetTrigger("FadeIn");
+
+        
+        if (_endingMusic != null)
+        {
+            _endingMusic.loop = true;
+            _endingMusic.Play();
         }
     }
 }
